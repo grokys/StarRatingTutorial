ec9473269eb9267af18e33d137b123b94b378568
Steven Kirk <grokys@gmail.com>
2020-03-06 16:41:02 +0100

    Add project files.

---
+++ StarRatingTutorial/StarRating.cs
@@ -0,0 +1,22 @@
+﻿using Avalonia;
+using Avalonia.Controls.Primitives;
+
+namespace StarRatingTutorial
+{
+    public class StarRating : TemplatedControl
+    {
+        public static readonly DirectProperty<StarRating, int> RatingProperty =
+            AvaloniaProperty.RegisterDirect<StarRating, int>(
+                nameof(Rating),
+                o => o.Rating,
+                (o, v) => o.Rating = v);
+
+        private int rating;
+
+        public int Rating
+        {
+            get => rating;
+            set => SetAndRaise(RatingProperty, ref rating, value);
+        }
+    }
+}
