4b21a7c9bb5be340cf6012a8cd4185acbe55a07b
Steven Kirk <grokys@gmail.com>
2020-03-06 17:02:48 +0100

    Display star rating by filling stars.

--- StarRatingTutorial/StarRating.cs
+++ StarRatingTutorial/StarRating.cs
@@ -1,5 +1,8 @@
 ﻿using Avalonia;
+using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
+using Avalonia.Controls.Shapes;
+using Avalonia.Media;
 
 namespace StarRatingTutorial
 {
@@ -12,11 +15,47 @@ namespace StarRatingTutorial
                 (o, v) => o.Rating = v);
 
         private int rating;
+        private Path[] stars;
 
         public int Rating
         {
             get => rating;
             set => SetAndRaise(RatingProperty, ref rating, value);
+        }
+
+        protected override void OnTemplateApplied(TemplateAppliedEventArgs e)
+        {
+            base.OnTemplateApplied(e);
+
+            stars = new[]
+            {
+                e.NameScope.Get<Path>("PART_One"),
+                e.NameScope.Get<Path>("PART_Two"),
+                e.NameScope.Get<Path>("PART_Three"),
+                e.NameScope.Get<Path>("PART_Four"),
+                e.NameScope.Get<Path>("PART_Five"),
+            };
+
+            UpdateStars();
+        }
+
+        protected override void OnPropertyChanged(AvaloniaPropertyChangedEventArgs e)
+        {
+            if (e.Property == RatingProperty)
+            {
+                UpdateStars();
+            }
+        }
+
+        private void UpdateStars()
+        {
+            if (stars != null)
+            {
+                for (var i = 0; i < stars.Length; ++i)
+                {
+                    stars[i].Fill = i < Rating ? Brushes.Orange : null;
+                }
+            }
         }
     }
 }
